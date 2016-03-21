71fc89720a75d3f670c36d8cca5b679b8fc25769
Alex Naidenko <alek-san@inbox.ru>
2016-03-21 23:20:22 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alek
+ * Date: 18.03.16
+ * Time: 22:29
+ */
+//sum=0;
+//foreach ($_POST as $value) {
+//    echo "<p>" . $value . "</p>";
+//}
+echo "sdfsdfsdf";
+echo "<br>";
+//echo "$_POST["sum"]";
+//$strHeading = $_POST["sum"];
+$a = $_POST['sum'];
+echo "jhjj" . $a;
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+echo "<br>";
+//if(isset($_POST['submit'])) echo $_POST['sum'];
+//$_POST['sum'] = 'value';
